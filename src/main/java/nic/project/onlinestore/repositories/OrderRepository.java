e25372f72f380f74ddffebf8f784d079aa063adb
TheBigBagrovski <matvey.bagr77@mail.ru>
2023-04-21 23:39:05 +0300

    Removed orders

--- src/main/java/nic/project/onlinestore/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package nic.project.onlinestore.repositories;
-
-import nic.project.onlinestore.models.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
